63ca9d2d73b3c14234fefd87fc2a28598c6f9084
huangweiliang83 <huangweiliang@gmail.com>
2016-10-30 14:37:35 +0800

    test

--- app.js
+++ app.js
@@ -62,8 +62,8 @@ fs.readFile(__dirname + '/carpark.xml', function(err, data) {
     parser.parseString(data, function (err, result) {
         console.dir(result);
         console.log('Done');
-        const util = require('util')
-    console.log(util.inspect(result, false, null))
+    //    const util = require('util')
+    //console.log(util.inspect(result, false, null))
     });
 });
 
