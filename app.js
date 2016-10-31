a0e4ebc80886fcefd041bb76eb73ed20245d0577
huangweiliang83 <huangweiliang@gmail.com>
2016-10-31 14:03:19 +0800

    test

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ fs.readFile(__dirname + '/carpark.xml', function(err, data) {
 var eyes = require('eyes');
 var https = require('https');
 var parser = new xml2js.Parser();
-var concat = require('concat-stream');
+//var concat = require('concat-stream');
 
 https.get('https://services2.hdb.gov.sg/webapp/BN22GetAmenitiesByRangeCoord/BN22SGetAmenitiesByRangeCoord?systemId=FI10&programID=MobileHDB&lngtd=103.848438&latd=1.332401&identifier=CPK&bounds=500', function(res) {
     var response_data = '';
