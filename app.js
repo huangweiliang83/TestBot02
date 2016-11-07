2b9d63917470931f8cefa58036e85f2d847d3614
huangweiliang83 <huangweiliang@gmail.com>
2016-11-07 10:22:13 +0800

    test

--- app.js
+++ app.js
@@ -477,7 +477,6 @@ function getnearestweather()
                     console.log("name 2: " + jsonobject2.channel.item[0].validTime);
                     //console.log("date : ", + jsonobject2.channel.item[0].forecastIssue.Child[0].date);
                     console.log("check : ", + jsonobject2.channel.item[0].validTime);
-                    
                     //
                     
                 
