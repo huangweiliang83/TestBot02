de9ea6e9d08a79604a3940fc34ed5000eed37e1f
huangweiliang83 <huangweiliang@gmail.com>
2016-11-07 15:31:40 +0800

    test

--- app.js
+++ app.js
@@ -433,7 +433,38 @@ function getnearestweather()
 {
     var forecastobj = {
         BR: "Mist",
-        CL: "Cloudy"
+        CL: "Cloudy",
+        CR: "Drizzle",
+        FA: "Fair(Day)",
+        FG: "Fog",
+        FN: "Fair(Night)",
+        FW: "Fair & Warm",
+        HG: "Heavy Thundery Showers with Gusty Winds",
+        HR: "Heavy Rain",
+        HS: "Heavy Showers",
+        HT: "Heavy Thundery Showers",
+        HZ: "Hazy",
+        LH: "Slightly Hazy",
+        LR: "Light Rain",
+        LS: "Light Showers",
+        OC: "Overcast",
+        PC: "Partly Cloudy (Day)",
+        PN: "Partly Cloudy (Night)",
+        PS: "Passing Showers",
+        RA: "Moderate Rain",
+        SH: "Showers",
+        SK: "Strong Winds,Showers",
+        SN: "Snow",
+        SR: "Strong Winds, Rain",
+        SS: "Snow Showers",
+        SU: "Sunny",
+        SW: "Strong Winds",
+        TL: "Thundery Showers",
+        WC: "Windy,Cloudy",
+        WD: "Windy",
+        WF: "Windy,Fair",
+        WR: "Windy,Rain",
+        WS: "Windy, Showers"
     };
 
     var parser1 = new xml2js.Parser({explicitArray : true, attrkey : 'Child'});
@@ -509,8 +540,8 @@ function getnearestweather()
                     console.log("Nearest Forecast : " + nearestForeCast);
                     console.log("Nearest Town : " + nearestForeCastName);
 
-                    var x = forecastobj["CL"];
-                    console.log("check forecast:" + x);
+                    var forecast = forecastobj[nearestForeCast];
+                    console.log("check forecast:" + forecast);
                 }
             });
         });
