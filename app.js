43e9437ba40cf85672e86c03a7f856d0a8dfa3ab
huangweiliang83 <huangweiliang@gmail.com>
2016-11-08 12:57:10 +0800

    test

--- app.js
+++ app.js
@@ -615,13 +615,13 @@ function callback(error, response, body) {
     var jsonobject3 = JSON.parse(body);
     console.log("parse data from URA");
     console.log(util.inspect(body, false, null));
-
+    console.log("carparknoURA: " + jsonobject3.Result[0].carparkNo);
   }
 }
 
 request(options, callback);
 
-console.log("carparknoURA: " + jsonobject3.Result[0].carparkNo);
+
 
 
 
