b65f4837cf3d9f176c7b444e527744d1e9db5c7f
huangweiliang83 <huangweiliang@gmail.com>
2016-11-08 12:39:03 +0800

    test

--- app.js
+++ app.js
@@ -603,7 +603,7 @@ getnearestweather('1.332401', '103.848438');
 
 var token = "c4f-7500yY7bc3h1f3Cf30vye1N45+sd8-yBsd4CrykdR25-WeWA+cq867Sx0-ce4FP3PrMv@P0cy5vvW37vcb63BgF38eGKj4A5";
 var options = {
-  url: 'https://www.ura.gov.sg/uraDataService/invokeUraDS?service=Car_ Park_Availability',
+  url: 'https://www.ura.gov.sg/uraDataService/invokeUraDS?service=Car_Park_Availability',
   headers: {
     'AccessKey' : '0d5cce33-3002-451b-8f53-31e8c4c54477',
     'Token' : token
