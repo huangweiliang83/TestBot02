14c097cf49d65122ab1f296d7fe7d7685debc8ab
huangweiliang83 <huangweiliang@gmail.com>
2016-11-09 15:49:50 +0800

    test

--- app.js
+++ app.js
@@ -848,7 +848,7 @@ getcarparkinformation('TPMD');
 getnearestweather('1.332401', '103.848438');
 getnearestURACarpark('1.332401', '103.848438');
 getnearestURACarparkInformation('K0087');
-getseasonparkinginformation('310100');
+getseasonparkinginformation('351101');
 
 
 
